3b6ddec8e2753fae9fa436a09677762893f54197
Sase <sase@stilsoft.net>
2021-09-29 10:36:07 +0200

    Implemented Enum extension methods to get all and custom enum attributes

--- StilSoft.Core/Extensions/EnumExtensions.cs
+++ StilSoft.Core/Extensions/EnumExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Reflection;
 
@@ -6,13 +7,28 @@ namespace StilSoft.Extensions
 {
     public static class EnumExtensions
     {
-        public static string GetDescription(this Enum enumObject)
+        public static string GetDescription(this Enum value)
         {
-            FieldInfo fieldInfo = enumObject.GetType().GetField(enumObject.ToString());
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
             DescriptionAttribute attribute = fieldInfo.GetCustomAttribute<DescriptionAttribute>();
-            string description = attribute == null ? enumObject.ToString() : attribute.Description;
+            string description = attribute == null ? value.ToString() : attribute.Description;
 
             return description;
+        }
+        public static TAttribute GetAttribute<TAttribute>(this Enum value) where TAttribute : Attribute
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+
+            return type.GetField(name).GetCustomAttribute<TAttribute>();
+        }
+
+        public static IEnumerable<Attribute> GetAttributes(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+
+            return type.GetField(name).GetCustomAttributes();
         }
     }
 }
